89932c59da67151f8aa5cb8cca3f93a94bd43771
Lucas Neuhaus <lucas.neuhaus93@gmail.com>
2020-03-15 18:45:41 -0300

    Added drawables and BindingAdapters

---
+++ app/src/main/java/com/example/tokenlabapp/BindingAdapters.kt
@@ -0,0 +1,62 @@
+package com.example.tokenlabapp
+
+import android.view.View
+import android.widget.ImageView
+import androidx.core.net.toUri
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.tokenlabapp.network.MoviesProperty
+import com.example.tokenlabapp.ui.overview.MovieListAdapter
+import com.example.tokenlabapp.ui.overview.MoviesApiStatus
+
+
+/**
+ * When there is no Movies property data (data is null), hide the [RecyclerView], otherwise show it.
+ */
+@BindingAdapter("listData")
+fun bindRecyclerView(recyclerView: RecyclerView, data: List<MoviesProperty>?) {
+    val adapter = recyclerView.adapter as MovieListAdapter
+    adapter.submitList(data)
+}
+
+/**
+ * Uses the Glide library to load an image by URL into an [ImageView]
+ */
+@BindingAdapter("imageUrl")
+fun bindImage(imgView: ImageView, imgUrl: String?) {
+    imgUrl?.let {
+        val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
+        Glide.with(imgView.context)
+            .load(imgUri)
+            .apply(
+                RequestOptions()
+                .placeholder(R.drawable.loading_animation)
+                .error(R.drawable.ic_broken_image))
+            .into(imgView)
+    }
+}
+
+/**
+ * This binding adapter displays the [MoviesApiStatus] of the network request in an image view.  When
+ * the request is loading, it displays a loading_animation.  If the request has an error, it
+ * displays a broken image to reflect the connection error.  When the request is finished, it
+ * hides the image view.
+ */
+@BindingAdapter("moviesApiStatus")
+fun bindStatus(statusImageView: ImageView, status: MoviesApiStatus?) {
+    when (status) {
+        MoviesApiStatus.LOADING -> {
+            statusImageView.visibility = View.VISIBLE
+            statusImageView.setImageResource(R.drawable.loading_animation)
+        }
+        MoviesApiStatus.ERROR -> {
+            statusImageView.visibility = View.VISIBLE
+            statusImageView.setImageResource(R.drawable.ic_connection_error)
+        }
+        MoviesApiStatus.DONE -> {
+            statusImageView.visibility = View.GONE
+        }
+    }
+}
